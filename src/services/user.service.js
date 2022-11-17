4d381765d387e6016adbf8c81cfbb0fc9ad70d6a
raphaelschnick <raphael.schnick@sybit.de>
2022-11-17 12:23:11 +0100

    change profile image

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -11,7 +11,16 @@ const userService = {
   },
   async changeDisplayName(displayName) {
     return await httpClient.put(`${END_POINT}`, {displayName})
-  }
+  },
+  async changeProfileImage(file) {
+    const formData = new FormData()
+    formData.append('file', file)
+    return await httpClient.post(`${END_POINT}/image`, formData, {
+      headers: {
+        'Content-Type': 'multipart/form-data'
+      }
+    })
+  },
 }
 
 export default userService
--- src/views/profile/Profile.vue
+++ src/views/profile/Profile.vue
@@ -1,37 +1,49 @@
 <template>
-<v-container>
-  <v-responsive class="d-flex align-center text-center fill-height" v-if="user">
-    <h1>Account</h1>
-    <v-avatar color="primary" size="130">
-      <h1>{{ user.username.at(0) + user.username.at(user.username.length - 1) }}</h1>
-    </v-avatar>
-    <v-row class="mt-3">
-      <v-col cols="12" md="6">
-        <h2>Benutzername</h2>
-        <h4>{{ user.username }}</h4>
-      </v-col>
-      <v-col>
-        <h2>Anzeigename</h2>
-        <div v-if="editDisplayName">
-          <v-text-field v-model="displayName" counter="30" required label="Name" variant="outlined" :rules="nameRules"></v-text-field>
-          <div class="d-flex justify-end">
-            <v-btn color="secondary" @click="editDisplayName = !editDisplayName">Abbrechen</v-btn>
-            <v-btn color="primary" @click="changeDisplayName" class="ml-3">Speichern</v-btn>
+  <v-container>
+    <v-responsive class="d-flex align-center text-center fill-height" v-if="user">
+      <h1>Account</h1>
+      <div class="img-container">
+        <v-avatar size="130" v-if="user.image" style="border: solid;">
+          <v-img :src="user.image.url"></v-img>
+        </v-avatar>
+        <v-avatar v-else>
+          <h1>{{ user.username.at(0) + user.username.at(user.username.length - 1) }}</h1>
+        </v-avatar>
+        <v-btn icon size="40" class="img-edit">
+          <v-icon @click="chooseFiles" size="20">mdi-pencil</v-icon>
+          <input id="fileUpload" type="file" @change="changeProfileImage" accept="image/*" hidden>
+        </v-btn>
+      </div>
+      <v-row class="mt-3">
+        <v-col cols="12" md="6">
+          <h2>Benutzername</h2>
+          <h4>{{ user.username }}</h4>
+        </v-col>
+        <v-col>
+          <h2>Anzeigename</h2>
+          <div v-if="editDisplayName">
+            <v-text-field v-model="displayName" counter="30" required label="Name" variant="outlined"
+                          :rules="nameRules"></v-text-field>
+            <div class="d-flex justify-end">
+              <v-btn color="secondary" @click="editDisplayName = !editDisplayName">Abbrechen</v-btn>
+              <v-btn color="primary" @click="changeDisplayName" class="ml-3">Speichern</v-btn>
+            </div>
           </div>
-        </div>
-        <div v-else>
-          <h4 class="ma-auto">{{ user.displayName }} <v-icon @click="editDisplayName = !editDisplayName" size="18">mdi-pencil</v-icon></h4>
-        </div>
-      </v-col>
-    </v-row>
-    <div class="mt-5">
-      <v-btn color="error" @click="$store.dispatch('logout')">
-        Abmelden
-      </v-btn>
-    </div>
-  </v-responsive>
-  <navigation />
-</v-container>
+          <div v-else>
+            <h4 class="ma-auto">{{ user.displayName }}
+              <v-icon @click="editDisplayName = !editDisplayName" size="18">mdi-pencil</v-icon>
+            </h4>
+          </div>
+        </v-col>
+      </v-row>
+      <div class="mt-5">
+        <v-btn color="error" @click="$store.dispatch('logout')">
+          Abmelden
+        </v-btn>
+      </div>
+    </v-responsive>
+    <navigation/>
+  </v-container>
 </template>
 
 <script>
@@ -65,11 +77,32 @@ export default {
           this.editDisplayName = false
         }
       }
+    },
+    chooseFiles() {
+      document.getElementById("fileUpload").click()
+    },
+    changeProfileImage(event) {
+      const files = event.target.files
+      userService.changeProfileImage(files[0]).then(response => {
+        if (response?.data?.id) {
+          this.user = response.data
+        }
+      })
     }
   }
 }
 </script>
 
 <style scoped>
+.img-container {
+  position: relative;
+  z-index: 0;
+}
 
+.img-edit {
+  position: absolute;
+  left: 56%;
+  bottom: -5px;
+  z-index: 1;
+}
 </style>
